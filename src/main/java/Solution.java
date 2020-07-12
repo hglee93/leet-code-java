31eb5e5b7f28d35608575b0d3f35125bfb5ca7b4
hglee93 <jeuslhg@gmail.com>
2020-07-12 22:25:20 +0900

    Add addTwoNumbers
    - LinkedList 개념을 알고 있어야 함
    - 캐리의 개념도 알고 있어야 함
    - T.C : O(n)
    - S.C : O(n)

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -32,4 +32,86 @@ public class Solution {
 
         return null;
     }
+
+    public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode cursor = null;
+        ListNode prevCursor = null;
+        ListNode head = null;
+
+        int carry = 0;
+
+        while(!(l1 == null || l2 == null)) {
+
+            int value = l1.val + l2.val + carry;
+
+            carry = value / 10;
+
+            cursor = new ListNode(value % 10);
+
+            if(prevCursor != null) { prevCursor.next = cursor; }
+            if(head == null) { head = cursor; }
+
+            l1 = l1.next;
+            l2 = l2.next;
+
+            prevCursor = cursor;
+            cursor = cursor.next;
+
+        }
+
+        if(l1 == null) {
+
+            while(l2 != null) {
+
+                int value = l2.val + carry;
+
+                carry = value / 10;
+                cursor = new ListNode(value % 10);
+
+                if(prevCursor != null) { prevCursor.next = cursor; }
+
+                if(head == null) { head = cursor; }
+
+                l2 = l2.next;
+
+                prevCursor = cursor;
+                cursor = cursor.next;
+            }
+
+        } else if(l2 == null) {
+
+            while(l1 != null) {
+                int value = l1.val + carry;
+
+                carry = value / 10;
+                cursor = new ListNode(value % 10);
+
+                if(prevCursor != null) { prevCursor.next = cursor; }
+
+                if(head == null) { head = cursor; }
+
+                l1 = l1.next;
+
+                prevCursor = cursor;
+                cursor = cursor.next;
+            }
+
+        }
+
+        if(carry == 1) {
+            cursor = new ListNode(1);
+            if(prevCursor != null) { prevCursor.next = cursor; }
+        }
+
+        return head;
+    }
 }
