5b71df90e56536550dab158cf097051625e6ea33
hglee93 <jeuslhg@gmail.com>
2020-07-12 22:23:32 +0900

    Add twoSum Solution
    - 조합을 이용
    - T.C : O(n^2)
    - S.C : O(1)

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -16,4 +16,20 @@ public class Solution {
 
         return answer;
     }
+
+    public int[] twoSum(int[] nums, int target) {
+
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(target == (nums[i] + nums[j])) {
+                    int[] answer = new int[2];
+                    answer[0] = i;
+                    answer[1] = j;
+                    return answer;
+                }
+            }
+        }
+
+        return null;
+    }
 }
