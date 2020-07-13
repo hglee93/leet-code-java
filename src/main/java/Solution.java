2aefe2c68e4378ce0c8d87eea3ca16c6adf5409a
hglee93 <jeuslhg@gmail.com>
2020-07-13 23:14:49 +0900

    Solved Same Tree

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -218,4 +218,30 @@ public class Solution {
 
         return dummyHead.next;
     }
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+
+    public boolean checkSameTree(TreeNode p, TreeNode q) {
+        if((p == null && q != null) || (p != null && q == null)) { return false; }
+        if(p == null && q == null) { return true; }
+        if(p.val != q.val) { return false; }
+        if(checkSameTree(p.left, q.left) == false) { return false; }
+        if(checkSameTree(p.right, q.right) == false) { return false; }
+        return true;
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return checkSameTree(p, q);
+    }
 }
