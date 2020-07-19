38d97048f20ba701c4eadb5f138981c8dfd7e63f
hglee93 <jeuslhg@gmail.com>
2020-07-19 20:25:15 +0900

    Solved addressbook by Programmers

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -569,4 +569,17 @@ public boolean isPalindrome(String s) {
 
         return sb.reverse().toString();
     }
+
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        for(int i = 0; i < phone_book.length; i++) {
+            for(int j = 0; j < phone_book.length; j++) {
+                if(i == j) { continue; }
+                if(phone_book[j].indexOf(phone_book[i]) == 0) {
+                    return false;
+                }
+            }
+        }
+        return answer;
+    }
 }
