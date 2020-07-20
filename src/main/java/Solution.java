215873dadac7810df56d03158a8b74cdbad584b4
hglee93 <jeuslhg@gmail.com>
2020-07-20 21:43:46 +0900

    Solved removeElements

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -641,4 +641,20 @@ public boolean isPalindrome(String s) {
         }
         return answer;
     }
+
+    public ListNode removeElements(ListNode head, int val) {
+
+        ListNode sentinel = new ListNode(0);
+        sentinel.next = head;
+
+        ListNode curr = head, prev = sentinel;
+
+        while(curr != null) {
+            if(curr.val == val) { prev.next = curr.next; }
+            else { prev = curr; }
+            curr = curr.next;
+        }
+
+        return sentinel.next;
+    }
 }
