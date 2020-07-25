b9ddd99b43fadbcc37c8276eceea5ca2af2b3fcd
hglee93 <jeuslhg@gmail.com>
2020-07-25 23:17:58 +0900

    Solved k번째 수 by Programmers

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -740,4 +740,17 @@ public boolean isPalindrome(String s) {
 
         return answer;
     }
+
+    public int[] kstnumber(int[] array, int[][] commands) {
+
+        int[] answer = new int[commands.length];
+
+        for(int i = 0; i < commands.length; i++) {
+            int[] arr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2] - 1];
+        }
+
+        return answer;
+    }
 }
