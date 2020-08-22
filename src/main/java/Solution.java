77bd93c3794858a4678fd4d69555aeb2c1fb8fbe
hglee93 <jeuslhg@gmail.com>
2020-08-22 17:33:54 +0900

    Solved Merge Intervals

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1068,4 +1068,47 @@ public boolean isPalindrome(String s) {
 
         return rtnArray;
     }
+
+    public int[][] merge(int[][] intervals) {
+
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+
+        if(intervals.length == 0) {
+            return new int[0][0];
+        }
+
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+
+        List<Integer> mergeInterval = new ArrayList<Integer>();
+        mergeInterval.add(intervals[0][0]);
+        mergeInterval.add(intervals[0][1]);
+
+        for(int i = 0; i < intervals.length - 1; i++) {
+            if(mergeInterval.get(1) >= intervals[i + 1][0]) {
+                int end = Math.max(mergeInterval.get(1), intervals[i + 1][1]);
+                mergeInterval.set(1, end);
+            }
+            else {
+                list.add(mergeInterval);
+                mergeInterval = new ArrayList<Integer>();
+                mergeInterval.add(intervals[i + 1][0]);
+                mergeInterval.add(intervals[i + 1][1]);
+            }
+        }
+
+        list.add(mergeInterval);
+
+        int[][] answer = new int[list.size()][2];
+
+        for(int i = 0; i < list.size(); i++) {
+            answer[i][0] = list.get(i).get(0);
+            answer[i][1] = list.get(i).get(1);
+        }
+
+        return answer;
+    }
 }
