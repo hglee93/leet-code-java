34667467bd51dc11fb94558978653bb681a2f9f7
hglee93 <jeuslhg@gmail.com>
2020-08-23 17:19:27 +0900

    Solved Subarray Sum Equals K

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1200,4 +1200,17 @@ public boolean isPalindrome(String s) {
 
         return answers;
     }
+
+    public int subarraySum(int[] nums, int k) {
+        int ans = 0;
+        for(int i = 0; i < nums.length; ++i) {
+            int sum = nums[i];
+            if(sum == k) { ans++; }
+            for(int j = i + 1; j < nums.length; ++j) {
+                sum += nums[j];
+                if(sum == k) { ans++; }
+            }
+        }
+        return ans;
+    }
 }
