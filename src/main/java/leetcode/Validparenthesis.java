6e45367319449ac18a1baa228f10426e5fbf069b
hglee93 <jeuslhg@gmail.com>
2021-01-09 11:31:20 +0900

    Solved Validparenthesis
    - 카운터가 아니라 스택을 써야하는 이유 : bracket의 상대적인 위치도 중요하기 때문에
    - if문으로 해둔 코드를 조금 더 다듬어 보자

---
+++ src/main/java/leetcode/Validparenthesis.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+import java.util.*;
+
+public class Validparenthesis {
+    public boolean isValid(String s) {
+        // Open bracket은 스택에 삽입하고
+        // Closed bracket일 경우 스택의 top을 확인한다.
+        // 왜 스택을 사용하는가?
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()) {
+
+            // 이 코드를 조금 더 다듬어 보자
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }
+            else if(c == ')' || c == '}' || c == ']') {
+
+                if(stack.size() == 0) { return false; }
+
+                if(c == ')' && stack.peek() != '(') {
+                    return false;
+                }
+
+                if(c == '}' && stack.peek() != '{') {
+                    return false;
+                }
+
+                if(c == ']' && stack.peek() != '[') {
+                    return false;
+                }
+
+                stack.pop();
+            }
+        }
+
+        if(stack.size() != 0) {
+            return false;
+        }
+
+        return true;
+    }
+}
