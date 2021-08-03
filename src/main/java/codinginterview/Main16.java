f7e7aa4411d43adacd112e512e35fdc47bf4b33b
hglee93 <jeuslhg@gmail.com>
2021-08-03 21:27:12 +0900

    solve

---
+++ src/main/java/codinginterview/Main16.java
@@ -0,0 +1,22 @@
+package codinginterview;
+
+public class Main16 {
+    String compress(String str) {
+
+        StringBuilder compressed = new StringBuilder();
+        int countConsecutive = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+
+            countConsecutive++;
+
+            if(i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+                compressed.append(str.charAt(i));
+                compressed.append(countConsecutive);
+                countConsecutive = 0;
+            }
+        }
+
+        return compressed.length() < str.length() ? compressed.toString() : str;
+    }
+}
